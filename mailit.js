e168702ac8196282256ba9697126e628b424d711
Ridhwaan Anayetullah <ra754@jla.rutgers.edu>
2019-06-24 11:33:35 -0400

    add startup logic

--- mailit.js
+++ mailit.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
-const initdir = './.mailit'
+const readline = require('readline');
+const nodemailer = require('nodemailer');
+const initdir = './.mailit';
 
 const args = process.argv.slice(2);
+
+const prefsJson = 'prefs.json';
+const historyJson = 'history.json';
 
 function Commit(message){
     this.message = message;
     this.counter = 0;
-    this.counter++;
 }
-Commit.prototype.commit= function(){}
+
+Commit.prototype.commit= function(){};
 Commit.prototype.simplehash = function(){
     let hash = 0;
     if(this.message.length == 0){
@@ -22,49 +27,69 @@ Commit.prototype.simplehash = function(){
     this.id = hash;
 }
 
-function isChanged(changes){
+function firstStart(){
  // go through all the files and see if any has changed
  const local = '.';
+ let foundHistory = false;
+ let foundPrefs = false;
+
  fs.readdir(local ,(err, files ) => {
      files.forEach(file => {
          fs.stat(file,(err,stats) => {
              if(err != null) console.log(err);
              if(stats.isFile()){
-                 console.log(file);
-                 console.log(stats);
+
+                 if(file.localeCompare(prefsJson) === 0){
+                     console.log("found existing prefs");
+                     foundPrefs = true;
+                 }
+
+                 if(file.localeCompare(historyJson) === 0){
+                     console.log("loading history");
+                     foundHistory = true;
+                 }
+
              }
          });
      });
  });
+
+// if history.json or prefs.json do not exists then this a first start.
+    return (foundHistory && foundPrefs)
 }
 
+async function update(param){
+    //
+}
+function showState(){
 
-async function serialize(){
-    // encode final linkedlist of commits into a JSON file
 }
 
-async function deserialize(){
-    // decode JSON file of commits into linkedlist
-}
-function update(param){
-    // update HEAD and push everything remote
-}
 function handleArgs(){
     const command = args[0];
     const param = args[1];
     console.log(command);
-    console.log('\n');
 
     switch(command){
+        case 'init':
+            if(!prefs())
+            {
+                console.log("Please put your email address in prefs.txt (first line)")
+            }
+
+            break;
         case 'update':
-            console.log('updating with message: ' + param)
+            console.log('updating with message: ' + param);
             update(param);
             break;
         case 'log':
             showLog();
             break;
+        case 'state':
+            showState();
+            break;
         case 'rollback':
-            console.log('rolling back to version ' + param)
+            console.log('rolling back to version ' + param);
             rollback(param);
             break;
         default:
@@ -73,4 +98,31 @@ function handleArgs(){
     }
 }
 
-handleArgs();
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function takeInput(question){
+    let ans = null;
+    rl.question(question, (answer) => {
+        // TODO: Log the answer in a database
+        ans = answer;
+        rl.close();
+    });
+    return ans;
+}
+
+let first = firstStart();
+if(first){
+    const optionsString = 'Welcome to mailit, a simple version control system through email. Please enter your email address.';
+    let email = takeInput(optionsString);
+    console.log(email);t
+    // write email to prefs
+    // writeEmailToPrefs();
+    // handleArgs(res);
+} else {
+
+}
+
