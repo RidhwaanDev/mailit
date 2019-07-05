0406879cb6e7ea3289e5a08442e252f4f6564b92
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2019-07-05 14:57:38 -0400

    minor changes

--- graph.js
+++ graph.js
@@ -1,7 +1,19 @@
-class Graph {
+class Node {
+    constructor({
+        this.#id= null;
+        this.data = null;
+        this.next = null;
+    })
+}
+class Graph{
     constructor() {
+        this.head = null;
+        this.next = null;
         super();
     }
-   // TODO implement graph
+   // TODO implement graph. As linkedlist
+    createList
 
 }
+
+
